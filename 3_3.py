a6e04c5e33596cfeceb99d205f9695b18169a864
TienPlusPluss <21222.nguyenthaongoclinh.dbk@gmail.com>
2025-10-14 10:00:20 +0000

    14/10/2025

---
+++ 3_3.py
@@ -0,0 +1,8 @@
+print("This program calculates and prints the sum of two numbers\n")
+
+firstNum = int(input("Enter the first number: "))
+secondNum = int(input("Enter the second number: "))
+
+sum = firstNum + secondNum
+
+print("The sum of two numbers is:", sum)
