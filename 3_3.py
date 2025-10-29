a6b636000d30acbae30ab232bcc2a24fc79477e9
octth25 <trungbg25102006@gmail.com>
2025-10-29 21:07:44 +0700

    Update

--- 3_3.py
+++
@@ -1,8 +0,0 @@
-print("This program calculates and prints the sum of two numbers\n")
-
-firstNum = int(input("Enter the first number: "))
-secondNum = int(input("Enter the second number: "))
-
-sum = firstNum + secondNum
-
-print("The sum of two numbers is:", sum)
