f017bad475b2f44f3fb51c4fc4de507ff663cb2f
nan56-sudo <nanhyarkiy@gmail.com>
2025-11-01 23:07:13 +0700

    feat: Cau hinh trang profile (edit profile, favorite)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/Login.jsx";
 import RegisterPage from "./pages/Register.jsx";
+import ProfilePage from "./pages/Profile.jsx";
 
 export default function App() {
   return (
     <Routes>
       {/* ✅ Trang mặc định khi truy cập "/" */}
-      <Route path="/" element={<Navigate to="/login" replace />} />
+      <Route path="/" element={<Navigate to="/profile" replace />} />
 
       {/* ✅ Trang login */}
       <Route path="/login" element={<LoginPage />} />
 
       {/* ✅ Trang signup */}
       <Route path="/signup" element={<RegisterPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+
     </Routes>
   );
 }
