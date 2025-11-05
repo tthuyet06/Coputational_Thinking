3af3a45eec3ffb3034f1686497919eb68d892ad7
Thanh Thuyêt <thanhthuyet102@gmail.com>
2025-11-05 18:46:29 +0700

    ..

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,16 +1,8 @@
  /** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
-<<<<<<< HEAD
-<<<<<<< HEAD
+     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+     theme: {
+         extend: {},
+     },
+     plugins: [],
  }
-=======
- }
->>>>>>> Gman
-=======
- }
->>>>>>> Gman
