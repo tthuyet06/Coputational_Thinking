6038305b353cec2a9e83e4ab2087e3e9746644e8
Chung Tien <chungga8436@gmail.com>
2025-12-08 20:41:56 +0700

    fix

--- backend/app/tests/test_recommendation.py
+++ backend/app/tests/test_recommendation.py
@@ -88,8 +88,8 @@ def test_db_driven_recommendation():
         mock_criteria = MockCriteria(
             location=user_location,
             duration_tag="#few_hours",
-            activities=["#streetfood"],
-            extra_tags=["#traditional"]
+            activities=["#movie"],
+            extra_tags=[]
         )
         mock_user = MockUser(id=1, hobbies=mock_criteria.extra_tags)
 
