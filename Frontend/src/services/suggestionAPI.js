0f65836e6367fadef7538e9f9dcfe9c7768de9b8
Gia Mẫn Phùng <giamanphung@Gmans-Mac.local>
2025-12-09 13:06:21 +0700

    merge main

--- Frontend/src/services/suggestionAPI.js
+++ Frontend/src/services/suggestionAPI.js
@@ -4,7 +4,6 @@ import api from "../api";
 const suggestionAPI = {
   // 1. Thêm 'activity' vào tham số nhận vào
   async getRecommendations({ latitude, longitude, duration_tag, activity }) {
-    
     // 2. Validate dữ liệu để khớp Schema
 
     // Schema yêu cầu: "duration_tag": "string"
@@ -16,13 +15,12 @@ const suggestionAPI = {
     const finalActivity = Array.isArray(activity) ? activity : [];
 
     // Gửi request
-    const res = await api.post("/api/v1/recommend/", {
+    const res = await api.post("/api/v1/recommend", {
       latitude: latitude,      // Schema: number -> OK
       longitude: longitude,    // Schema: number -> OK
       duration_tag: finalDurationTag, // Schema: string -> Đã ép kiểu
       activity: finalActivity, // Schema: [string] -> OK (Lấy từ tham số truyền vào)
     });
-
     return res.data?.recommendations ?? [];
   },
 };
